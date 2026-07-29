fd9d3eeb87154ec895609ead02bfc45b87027659
jwss <jwssstay@gmail.com>
2026-07-29 19:44:04 +0530

    feat: update map embed to show color and highlight Woodside Serene

--- src/components/sections/Location.tsx
+++ src/components/sections/Location.tsx
@@ -56,14 +56,14 @@ export function Location() {
             {/* A placeholder for the map. We'll use a local landscape image or an actual iframe */}
             <div className="absolute inset-0 bg-woodside-800 animate-pulse"></div>
             <iframe 
-              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d15582.464619736801!2d78.7849!3d12.5693!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zMTLCsDM0JzA5LjUiTiA3OMKwNDcnMDUuNiJF!5e0!3m2!1sen!2sin!4v1630000000000!5m2!1sen!2sin" 
+              src="https://maps.google.com/maps?q=Woodside+Serene+Farm+Stay,+Kavalur,+Jawadhu+Hills&t=&z=14&ie=UTF8&iwloc=&output=embed" 
               width="100%" 
               height="100%" 
               style={{ border: 0 }} 
               allowFullScreen={true} 
               loading="lazy" 
               referrerPolicy="no-referrer-when-downgrade"
-              className="absolute inset-0 z-10 grayscale hover:grayscale-0 transition-all duration-700 opacity-90 hover:opacity-100"
+              className="absolute inset-0 z-10 w-full h-full"
             ></iframe>
           </div>
           
